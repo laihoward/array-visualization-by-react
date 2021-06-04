f34efe18a43e307c7c8e73ac671d34e3583f4aa1
Pin Hao Lai <howardlai30425@gmail.com>
2021-06-04 18:14:00 +0800

    show data by circle

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { Input,Button } from 'antd';
+import Bar from './component/square'
+class App extends React.Component{
+  state={
+    array:[7,5,12],
+    inputValue:''
+  }
+  render(){
+    let circleDiv = this.state.array.map((value,index)=><Bar
+      key={index}  
+      data={value}
+    />)
+    return(
+      
+      <div className ="App">
+        <h1>array visualization</h1>
+        <h2>push number between 0 to 99</h2>
+        <section>
+          <Input
+            className="inputData"
+            value={this.state.inputValue}
+            // onChange={this.handleInputChange} 
+          />
+          <button   
+              className="pushBtn"
+              // onClick={this.changeToBubbleSort}
+          >Push</button>
+          <button 
+              className="clearBtn"
+              // onClick={this.changeToMergeSor}
+          >Clear</button>
+          <button 
+              className="sortingBtn"
+              // onClick={this.changeToQuickSort}
+          >Sorting</button>
+        </section>
+        <section className="circle card container">
+          {circleDiv}
+        </section>
+      </div>
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    )
+  }
 }
-
 export default App;
